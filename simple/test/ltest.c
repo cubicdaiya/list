6f9824211fb818227881cf3ccc2e904977d6fc59
cubicdaiya <cubicdaiya@gmail.com>
2010-01-08 22:42:12 +0900

    add test

--- simple/test/ltest.c
+++ simple/test/ltest.c
@@ -9,6 +9,7 @@ void test_list_add(void);
 void test_list_addfront(void);
 void test_list_add_addfront(void);
 void test_list_get(void);
+void test_list_check_head(void);
 
 void test_list_add(void) {
   list_t *l = list_create();
@@ -77,6 +78,19 @@ void test_list_get(void) {
   list_destroy(l);
 }
 
+void test_list_check_head(void) {
+  list_t *l = list_create();
+  for (int i=0;i<NUM;++i) {
+    l = list_add(l, i+1);
+  }
+  list_t *h = l->head;
+  for (list_t *p=l->head;p!=NULL;p=p->next) {
+    CU_ASSERT(p->head == h);
+  }
+  list_destroy(l);
+}
+
+
 int main (int argc, char *argv[]) {
   CU_pSuite suite_list;
   CU_initialize_registry();
@@ -85,6 +99,7 @@ int main (int argc, char *argv[]) {
   CU_add_test(suite_list, "test_list_addfront",     test_list_addfront);
   CU_add_test(suite_list, "test_list_add_addfront", test_list_add_addfront);
   CU_add_test(suite_list, "test_list_get",          test_list_get);
+  CU_add_test(suite_list, "test_list_check_head",   test_list_check_head);
   CU_console_run_tests();
   CU_cleanup_registry();
   
